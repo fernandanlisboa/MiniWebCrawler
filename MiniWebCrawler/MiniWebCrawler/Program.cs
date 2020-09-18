7749eb4038e44e970216218ff03e5ff9ac2a25f5
fernandanlisboa <fernandanlisboa@gmail.com>
2020-09-18 09:27:48 -0300

    initial commit

---
+++ MiniWebCrawler/MiniWebCrawler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniWebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
